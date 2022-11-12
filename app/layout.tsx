e735b7757c3754d14c79172ba0936ec3e1f0e3f4
youngbloodcyb <cameron.youngblood@gmail.com>
2022-11-12 15:42:45 -0700

    add notes directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 export default function RootLayout({
   children,
 }: {
@@ -6,7 +8,15 @@ export default function RootLayout({
   return (
     <html>
       <head />
-      <body>{children}</body>
+        <body>
+          <main>
+            <nav>
+              <Link href="/">Home</Link>
+              <Link href="/notes">Notes</Link>
+            </nav>
+          {children}
+          </main>
+        </body>
     </html>
   )
 }
