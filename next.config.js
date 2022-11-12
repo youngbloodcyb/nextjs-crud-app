c2b805355649f7aa6858f2ec226418c04c263a88
youngbloodcyb <cameron.youngblood@gmail.com>
2022-11-12 15:36:58 -0700

    update next.config with experimental config and add base component

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
 }
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  experimental: {
+    appDir: true
+  }  
+}
