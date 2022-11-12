67687470dcf4766a7fbeda45c997e3e56f27e91c
youngbloodcyb <cameron.youngblood@gmail.com>
2022-11-12 16:03:16 -0700

    add dynamic slugs and note pages

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,7 +1,23 @@
-export default async function NotePage() {
+async function getNote(noteId: string) {
+    const res = await fetch(
+        `http://127.0.0.1:8090/api/collections/notes/records/${noteId}`, {
+            next: { revalidate: 10 }
+        }
+    );
+    const data = await res.json();
+    return data;
+}
+
+export default async function NotePage({ params }: any) {
+    const note = await getNote(params.id);
     return (
         <div>
             <h1>Note</h1>
+            <div>
+                <h3>{note.title}</h3>
+                <h5>{note.content}</h5>
+                <p>{note.created}</p>
+            </div>
         </div>
     );
 }
