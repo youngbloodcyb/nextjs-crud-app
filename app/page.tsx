77640ab46c1d7e82e4dbf78220f459ebb226a1d6
youngbloodcyb <cameron.youngblood@gmail.com>
2022-11-12 16:33:04 -0700

    add tailwind css

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
     return (
         <div>
-            <h1>Home Page</h1>
+            <h1 className="text-3xl">Home Page</h1>
             <p>Some Content</p>
         </div>
     );
