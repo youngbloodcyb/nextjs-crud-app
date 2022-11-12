8064eab0be06c41bc5ddc904841e36094c50d91a
youngbloodcyb <cameron.youngblood@gmail.com>
2022-11-12 16:37:03 -0700

    need to fix tailwind

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -12,7 +12,7 @@ export default async function NotePage({ params }: any) {
     const note = await getNote(params.id);
     return (
         <div>
-            <h1>Note</h1>
+            <h1 className="text-3xl">Note</h1>
             <div>
                 <h3>{note.title}</h3>
                 <h5>{note.content}</h5>
