1485199c261f9386ad8984b1ef0fc7f9fc537cbe
youngbloodcyb <cameron.youngblood@gmail.com>
2022-11-14 22:27:02 -0700

    update daisyUI theme & add nav component

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,14 +7,26 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html>
+    <html data-theme="retro">
       <head />
         <body>
           <main>
             <nav>
-              <Link href="/">Home</Link>
-              <Link href="/notes">Notes</Link>
+            <div className="navbar bg-base-100">
+              <div className="flex-1">
+                  <Link href="/" className="btn btn-ghost normal-case text-xl">Home</Link>
+              </div>
+              <div className="flex-none">
+                <ul className="menu menu-horizontal p-0">
+                  <li><Link href="/notes">Notes</Link></li>
+                  <li tabIndex={0}>
+                  </li>
+                  <li><a>Coming soon...</a></li>
+                </ul>
+              </div>
+            </div>
             </nav>
+            
           {children}
           </main>
         </body>
