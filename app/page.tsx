57531002c359b90a663c5ad28c3226f5d7f855c5
youngbloodcyb <cameron.youngblood@gmail.com>
2022-11-14 22:33:45 -0700

    update style on home

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ export default function HomePage() {
         <div className="w-screen flex flex-col justify-center items-center">
             <div className="w-1/2 my-20">
                 <div className="mockup-code">
-                    <pre><code>without prefix</code></pre>
+                    <pre><code>My Note App...</code></pre>
+                    <pre><code>A simple note-taking app built with Next.js, pocketbase, and daisyUI.</code></pre>
                 </div>
             </div>
         </div>
